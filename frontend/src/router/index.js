982ea82f5e0d23fb6d37205147de88d879de0a7e
jihoon-ko <jihoonko@kaist.ac.kr>
2017-07-30 09:50:38 +0000

    just design

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Hello from '../components/Hello.vue';
 import CreateRoom from '../components/CreateRoom.vue';
+import Room from '../components/Room.vue';
+import UserView from '../components/UserView.vue';
 
 Vue.use(Router);
 
@@ -17,6 +19,16 @@ export default new Router({
       path: '/create',
       name: 'CreateRoom',
       component: CreateRoom,
+    },
+    {
+      path: '/room/:roomId',
+      name: 'Room',
+      component: Room,
+    },
+    {
+      path: '/user/:userId',
+      name: 'User',
+      component: UserView,
     },
   ],
 });
