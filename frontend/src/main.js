16d49f1bc1c355779acc0f569b9af3e0ff1582c1
Jihoon Ko <jihoonko@kaist.ac.kr>
2017-08-01 04:05:21 +0900

    add vue-socket link for verify room and user (#6)

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -14,8 +14,8 @@ import store from './store'; // vuex store instance
 Vue.use(VueMaterial);
 // Vue.use(pdf);
 Vue.use(VueAxios, axios);
-// Vue.use(VueSocketio, socketio('http://52.78.113.241:3000'), store);
-Vue.use(VueSocketio, socketio('http://localhost:3000'), store);
+Vue.use(VueSocketio, socketio('http://52.78.113.241:3000'), store);
+// Vue.use(VueSocketio, socketio('http://localhost:3000'), store);
 
 
 Vue.config.productionTip = false;
